eeea5bd885d604bc551580461fdfdf9cad0db1be
Peter Bengtsson <mail@peterbe.com>
2025-07-10 21:37:38 -0400

    getback command (#1)
    * getback command
    
    * remove print
    
    * getback command
    
    * getback command
    
    * getback command
    
    * getback command
    
    * small fix

--- src/commit-branch.ts
+++ src/commit-branch.ts
@@ -1,5 +1,6 @@
-import { checkbox, input } from "@inquirer/prompts"
+import { checkbox, confirm, input } from "@inquirer/prompts"
 import simpleGit, { type SimpleGit } from "simple-git"
+import { getDefaultBranch } from "./branch-utils"
 import { getHumanAge } from "./human-age"
 import { success, warn } from "./logger"
 import { getTitle } from "./storage"
@@ -9,6 +10,7 @@ type Options = {
   yes?: boolean
 }
 export async function commitBranch(options: Options) {
+  const yes = Boolean(options.yes)
   const noVerify = !options.verify
   const git = simpleGit()
   const branchSummary = await git.branch()
@@ -17,7 +19,7 @@ export async function commitBranch(options: Options) {
   const defaultBranch = await getDefaultBranch(git)
   if (defaultBranch === currentBranch) {
     throw new Error(
-      `You are on the default branch (${defaultBranch}). Please switch to a feature branch before committing.`,
+      `You are on the default branch (${defaultBranch}). Switch to a feature branch before committing.`,
     )
   }
 
@@ -26,16 +28,16 @@ export async function commitBranch(options: Options) {
     warn("\nUntracked files:")
     await printUnTrackedFiles(untrackedFiles)
     console.log("")
-    const confirm = await input({
+    const confirmed = await input({
       message: "Do you want to add these untracked files? [y/n/i]",
     })
-    if (confirm.toLowerCase() === "n") {
+    if (confirmed.toLowerCase() === "n") {
       // throw new Error(
       //   "Untracked files found. Please add them before committing."
       // );
-    } else if (confirm.toLowerCase() === "y") {
+    } else if (confirmed.toLowerCase() === "y") {
       await git.add(untrackedFiles)
-    } else if (confirm.toLowerCase() === "i") {
+    } else if (confirmed.toLowerCase() === "i") {
       // Interactive
       const choices: { name: string; value: string }[] = []
       for (const name of untrackedFiles) {
@@ -69,57 +71,89 @@ export async function commitBranch(options: Options) {
   const remotes = await git.getRemotes(true) // true includes URLs
   const origin = remotes.find((remote) => remote.name === "origin")
   const originUrl = origin ? origin.refs.fetch : null // or origin.refs.push
-  const originName = originUrl
+  const originName = origin ? origin.name : "origin"
 
-  let pushToRemote = Boolean(options.yes)
+  let pushToRemote = yes
   if (!pushToRemote && origin) {
-    const askedPushToRemote = await input({
-      message: `Push to ${originName}: [Y/n]`,
+    pushToRemote = await confirm({
+      message: `Push to ${originName}:`,
+      default: true,
     })
-    if (askedPushToRemote.toLowerCase() === "n") {
-      pushToRemote = false
-    } else if (
-      askedPushToRemote === "" ||
-      askedPushToRemote.toLowerCase() === "y"
-    ) {
-      pushToRemote = true
-    } else {
-      throw new Error("Invalid input. Please enter 'Y' or 'n'.")
-    }
+    // if (askedPushToRemote.toLowerCase() === "n") {
+    //   pushToRemote = false;
+    // } else if (
+    //   askedPushToRemote === "" ||
+    //   askedPushToRemote.toLowerCase() === "y"
+    // ) {
+    //   pushToRemote = true;
+    // } else {
+    //   throw new Error("Invalid input. Please enter 'Y' or 'n'.");
+    // }
   }
 
   const unstagedFiles = await getUnstagedFiles(git)
-  console.log("unstagedFiles:", unstagedFiles)
   await git.add(unstagedFiles)
 
-  await git.commit(title, noVerify ? ["--no-verify"] : undefined)
-  if (pushToRemote) {
-    await git.push("origin", currentBranch)
-    success(`Changes pushed to ${originName}/${currentBranch}`)
-  } else {
-    success("Changes committed but not pushed.")
+  try {
+    await git.commit(title, noVerify ? ["--no-verify"] : undefined)
+    if (pushToRemote) {
+      await git.push("origin", currentBranch)
+      success(`Changes pushed to ${originName}/${currentBranch}`)
+    } else {
+      success("Changes committed but not pushed.")
+    }
+    const nwo = pushToRemote && originUrl && getGitHubNWO(originUrl)
+    if (nwo) {
+      // e.g. https://github.com/peterbe/admin-peterbecom/pull/new/upgrade-playwright
+
+      success(`https://github.com/${nwo}/pull/new/${currentBranch}`)
+      console.log("(⌘-click to open URLs)")
+    }
+  } catch (error) {
+    if (
+      yes &&
+      error instanceof Error &&
+      error.message.includes("nothing to commit, working tree clean")
+    ) {
+      // console.log("ERROR", error)
+      // console.log("ERROR.NAME", error.name)
+      await git.push("origin", currentBranch)
+      success(`Changes pushed to ${originName}/${currentBranch}`)
+    } else {
+      throw error
+    }
   }
-  warn("Need to print URL for creating new gitHub pr")
+}
+
+function getGitHubNWO(url: string): string | undefined {
+  // E.g. git@github.com:peterbe/admin-peterbecom.gi
+  // or https://github.com/peterbe/admin-peterbecom.git"
+  if (url.includes("github.com")) {
+    if (URL.canParse(url)) {
+      const parsed = new URL(url)
+      return parsed.pathname.replace(/\.git$/, "").slice(1)
+    }
+    if (url.includes("git@github.com:")) {
+      const second = url.split(":")[1]
+      if (second) {
+        return second.replace(/\.git$/, "")
+      }
+    } else {
+      throw new Error(`Not implemented (${url})`)
+    }
+  }
+  return url
 }
 
 async function getUntrackedFiles(git: SimpleGit): Promise<string[]> {
   const status = await git.status()
-  return status.not_added
+  return status.not_added.filter((file) => !file.endsWith("~"))
 }
 
 async function getUnstagedFiles(git: SimpleGit): Promise<string[]> {
   const status = await git.status()
   // console.log("Unstaged files:", status.staged);
   return status.modified
-}
-
-async function getDefaultBranch(git: SimpleGit) {
-  const branches = await git.branch(["-r"])
-  const defaultBranch = branches.all
-    .find((branch) => branch.includes("origin/HEAD -> origin/"))
-    ?.split("origin/HEAD -> origin/")[1]
-
-  return defaultBranch || "main"
 }
 
 async function printUnTrackedFiles(files: string[]) {
