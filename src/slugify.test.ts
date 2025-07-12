c5d44b75a55d31b947b00a34f52e321b61c0092c
Peter Bengtsson <mail@peterbe.com>
2025-07-12 17:41:37 -0400

    print pre-commits better

--- src/slugify.test.ts
+++ src/slugify.test.ts
@@ -1,10 +1,10 @@
-import { describe, expect, test } from "bun:test";
-import { slugifyTitleToBranchName } from "./slugify";
+import { describe, expect, test } from "bun:test"
+import { slugifyTitleToBranchName } from "./slugify"
 
 describe("slugify title to branch name", () => {
   test("keeps forward slashes", () => {
     expect(slugifyTitleToBranchName("peterbe/CAP-123 Bla bla")).toBe(
-      "peterbe/cap-123-bla-blaXXX"
-    );
-  });
-});
+      "peterbe/cap-123-bla-bla",
+    )
+  })
+})
