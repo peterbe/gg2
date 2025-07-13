e211d089045c6980526f7b20f5be3aea52f0618b
Peter Bengtsson <mail@peterbe.com>
2025-07-12 21:32:40 -0400

    nicer pr status

--- src/github.ts
+++ src/github.ts
@@ -75,4 +75,7 @@ export async function gitHubPR() {
   const { message, canMerge } = interpretMergeableStatus(prDetails)
   if (canMerge) success(message)
   else warn(message)
+
+  console.log(prDetails)
+  console.log("auto_merge:", prDetails.auto_merge)
 }
