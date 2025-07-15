87d73cbe164cb674cb61ee3274fb85b97d6f81ce
Peter Bengtsson <mail@peterbe.com>
2025-07-15 13:23:19 -0400

    Fix for finding the default branch

--- src/branch-utils.ts
+++ src/branch-utils.ts
@@ -1,6 +1,14 @@
 import type { SimpleGit } from "simple-git"
 
 export async function getDefaultBranch(git: SimpleGit) {
+  const result = await git.raw(["symbolic-ref", "refs/remotes/origin/HEAD"])
+  if (result) {
+    const defaultBranch = result.trim().replace("refs/remotes/origin/", "")
+    if (defaultBranch) {
+      return defaultBranch
+    }
+  }
+
   const branches = await git.branch(["-r"])
   const defaultBranch = branches.all
     .find((branch) => branch.includes("origin/HEAD -> origin/"))
