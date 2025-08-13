e561ef56aa278380647db66022240d418210ab5e
Peter Bengtsson <mail@peterbe.com>
2025-08-13 07:42:47 -0400

    Shell completion (#30)

---
+++ src/completion.sh
@@ -0,0 +1,42 @@
+_generate_foo_completions() {
+  local idx=$1; shift
+  local words=( "$@" )
+  local current_word=${words[idx]}
+
+  gg shell-completion --list "${words}"
+}
+
+_complete_foo_bash() {
+  local IFS=$'\n'
+  local raw=($(_generate_foo_completions "$COMP_CWORD" "${COMP_WORDS[@]}"))
+  local trimmed=()
+  trimmed+=( "${raw[@]}" )
+
+  if (( ${#raw[@]} == 1 )); then
+    trimmed+=( "${raw[0]%%:*}" )
+  fi
+
+  COMPREPLY=( "${trimmed[@]}" )
+}
+
+_complete_foo_zsh() {
+  local -a raw trimmed
+  local IFS=$'\n'
+  raw=($(_generate_foo_completions "$CURRENT" "${words[@]}"))
+
+  for d in $raw; do trimmed+=( "${d%%:*}" ); done
+  if (( ${#raw} == 1 )); then
+    trimmed+=( "${raw[1]}" )
+    raw+=( "${trimmed[1]}" )
+  fi
+
+  compadd -d raw -- $trimmed
+}
+
+if [ -n "${ZSH_VERSION:-}" ]; then
+  autoload -Uz compinit
+  compinit
+  compdef _complete_foo_zsh gg
+elif [ -n "${BASH_VERSION:-}" ]; then
+  complete -F _complete_foo_bash gg
+fi
---
+++ src/shell-completion.ts
@@ -0,0 +1,112 @@
+/**
+ * As a tip for development/debugging, to test this outside of bash/zsh
+ * you can simulate what the bash/zsh script does by using this notation,
+ * for example:
+ *
+ *  gg shell-completion --list "gg\ncommit\n"
+ *
+ */
+
+import type { Command } from "commander"
+
+// @ts-ignore - a trick to make a non TS file part of memory and build
+import code from "./completion.sh" with { type: "text" }
+
+export async function shellCompletion() {
+  console.log(code)
+}
+
+type Options = {
+  search?: string
+  program: Command
+}
+
+export async function printCompletions(options: Options) {
+  const { program } = options
+  // This search, if not undefined, will be a multi-line string always
+  // starts with the word "gg". For example, if the user has typed
+  // `gg [TAB]` what's sent here is that search === "gg\n".
+  const words = (options.search || "").split("\n")
+  words.shift()
+
+  if (words.length === 1) {
+    const word = words[0]
+    const names: string[] = []
+    for (const command of program.commands) {
+      if (command.name() === "shell-completion") continue
+      names.push(command.name())
+      for (const alias of command.aliases()) {
+        names.push(alias)
+      }
+    }
+    names.sort((a, b) => a.localeCompare(b))
+    console.log(
+      names
+        .filter((name) => {
+          return (word && name.includes(word)) || true
+        })
+        .join("\n"),
+    )
+  } else if (words.length === 2) {
+    // E.g. "start -"
+    const word = words[0]
+    for (const command of program.commands) {
+      if (command.name() === "shell-completion") {
+        continue
+      }
+      if (word === command.name()) {
+        const flags: string[] = []
+        for (const option of command.arguments(word).options) {
+          if (option.long) flags.push(option.long)
+          if (option.short) flags.push(option.short)
+        }
+
+        const commands = command.commands.map((command) => command.name())
+
+        console.log(
+          [...commands, ...flags]
+            .filter((flag) => {
+              if (words[1]) {
+                return flag.startsWith(words[1])
+              }
+              return true
+            })
+            .join("\n"),
+        )
+      }
+    }
+  } else if (words.length === 3) {
+    // E.g. "gg github pr "
+    const word = words[0]
+    const subword = words[1]
+    for (const command of program.commands) {
+      if (word === command.name()) {
+        for (const subcommand of command.commands) {
+          if (subcommand.name() === subword) {
+            const flags: string[] = []
+            for (const option of subcommand.arguments(word).options) {
+              if (option.long) flags.push(option.long)
+              if (option.short) flags.push(option.short)
+            }
+
+            const commands = subcommand.commands.map((command) =>
+              command.name(),
+            )
+            console.log(
+              [...commands, ...flags]
+                .filter((flag) => {
+                  if (words[2]) {
+                    return flag.startsWith(words[2])
+                  }
+                  return true
+                })
+                .join("\n"),
+            )
+          }
+        }
+      }
+    }
+  } else {
+    // Currently not supported.
+  }
+}
