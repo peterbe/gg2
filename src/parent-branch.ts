2d3fd2bd010e21f1341e9e7d4f7d7c76629b281b
Peter Bengtsson <mail@peterbe.com>
2025-08-15 11:20:47 -0400

    sample branch, delete me after

--- src/parent-branch.ts
+++ src/parent-branch.ts
@@ -1,9 +1,11 @@
+import kleur from "kleur"
 import simpleGit from "simple-git"
+import { warn } from "./logger"
 
 export async function parentBranch() {
   const git = simpleGit()
   const branchSummary = await git.branch()
-  const _currentBranch = branchSummary.current
+  const currentBranch = branchSummary.current
 
   const outputFromGit = (
     await simpleGit({ trimmed: true }).raw("show-branch", "-a")
@@ -14,33 +16,19 @@ export async function parentBranch() {
     "HEAD",
   )
 
-  console.log(
-    outputFromGit
-      .map((line) => line.replace(/\].*/, "")) // remove branch commit message
-      .filter((line) => line.includes("*")) // only lines with a star in them
-      .filter((line) => !line.includes(rev)) // only lines not including the specified revision
-      .filter((_line, index, all) => index < all.length - 1) // not the last line
-      .map((line) => line.replace(/^.*\[/, "")), // remove all but the branch name
-  )
-  //   const outputFromGit = (
-  //     await simpleGit({ trimmed: true }).raw("show-branch", "-a")
-  //   ).split("\n")
-  //   const rev = await simpleGit({ trimmed: true }).raw(
-  //     "rev-parse",
-  //     "--abbrev-ref",
-  //     "HEAD",
-  //   )
-  //   const allLinesNormalized = outputFromGit.map((line) =>
-  //     line.trim().replace(/\].*/, ""),
-  //   )
-  //   const indexOfCurrentBranch = allLinesNormalized.indexOf(`* [${rev}`)
-  //   if (indexOfCurrentBranch > -1) {
-  //     const parentBranch = allLinesNormalized[indexOfCurrentBranch + 1].replace(
-  //       /^.*\[/,
-  //       "",
-  //     )
-  // console.log(
-  //   `Parent branch for ${currentBranch} is: ${kleur.bold(parentBranch)}`,
-  // )
-  //   }
+  const parents = outputFromGit
+    .map((line) => line.replace(/\].*/, "")) // remove branch commit message
+    .filter((line) => line.includes("*")) // only lines with a star in them
+    .filter((line) => !line.includes(rev)) // only lines not including the specified revision
+    .filter((_line, index, all) => index < all.length - 1) // not the last line
+    .map((line) => line.replace(/^.*\[/, "")) // remove all but the branch name
+
+  if (parents.length >= 1 && parents[0]) {
+    const parentBranch = parents[0]
+    console.log(
+      `Parent branch for ${currentBranch} is: ${kleur.bold(parentBranch)}`,
+    )
+  } else {
+    warn("Unable to find any parents")
+  }
 }
