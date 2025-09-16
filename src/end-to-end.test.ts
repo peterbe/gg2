5090d1c8f680446c3dd775414694a11cd56694b3
Peter Bengtsson <mail@peterbe.com>
2025-09-16 16:29:24 -0400

    Basic end-to-end tests (#50)
    * Basic end-to-end tests
    
    * debug path
    
    * x64
    
    * pwd and whoami
    
    * compile and install
    
    * try again
    
    * try copy instead
    
    * debugging
    
    * debugging
    
    * debugging
    
    * create first
    
    * try again
    
    * try again
    
    * more testing
    
    * tidying up a bit

---
+++ src/end-to-end.test.ts
@@ -0,0 +1,24 @@
+import { afterEach, beforeEach, describe, expect, test } from "bun:test"
+import { mkdtemp, rm } from "node:fs/promises"
+import { tmpdir } from "node:os"
+import { join } from "node:path"
+import { $ } from "bun"
+
+describe("basics", async () => {
+  let tempDir = ""
+  beforeEach(async () => {
+    tempDir = await mkdtemp(join(tmpdir(), "myapp-"))
+  })
+  afterEach(async () => {
+    await rm(tempDir, { recursive: true })
+  })
+
+  test("version", async () => {
+    const output = await $`gg --version`.text()
+    expect(/\d+\.\d+\d+/.test(output)).toBe(true)
+  })
+  test("help", async () => {
+    const output = await $`gg --help`.text()
+    expect(/Usage:/.test(output)).toBe(true)
+  })
+})
